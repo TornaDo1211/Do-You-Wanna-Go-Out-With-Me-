ac1b61f09f1ea81357a0c37be866decbf6b58b2a
TornaDo1211 <ilayk06@gmail.com>
2025-02-02 23:47:24 +0200

    change it

---
+++ script.js
@@ -0,0 +1,27 @@
+function moveButton() {
+    const noButton = document.querySelector('#noButton');
+
+    const rect = noButton.getBoundingClientRect();
+    const oldX = rect.left + window.scrollX;
+    const oldY = rect.top + window.scrollY;
+
+    const maxX = window.innerWidth - noButton.offsetWidth;
+    const maxY = window.innerHeight - noButton.offsetHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    noButton.style.position = "absolute";
+    noButton.style.left = `${randomX}px`;
+    noButton.style.top = `${randomY}px`;
+
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    dot.style.left = `${oldX}px`;
+    dot.style.top = `${oldY}px`;
+    document.body.appendChild(dot);
+
+    setTimeout(() => {
+        dot.remove();
+    }, 1000);
+}
